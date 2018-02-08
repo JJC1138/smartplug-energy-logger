6333c71ff49ba1473c44e4fbc9a9d4d37726fe36
Jon Colverson <github@vcxz.co.uk>
2018-02-08 22:49:02 +0000

    Added parsing of an '--interval' option using commander, and added the beginnings of device polling

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 #!/usr/bin/env node
 
+const program = require('commander');
 const { Client } = require('tplink-smarthome-api');
- 
+
+program
+  .option('--interval <n>', 'The time in seconds between readings', parseFloat, 0.5)
+  .parse(process.argv);
+
 const client = new Client();
 
-client.startDiscovery().on('device-new', (device) => {
-  device.getSysInfo().then(console.log);
+let plugs = [];
+
+function poll() {
+  for (let plug of plugs) {
+    // FIXME implement
+    console.log(plug.alias);
+  }
+}
+
+client.startDiscovery().on('plug-new', (plug) => {
+  plugs.push(plug);
+
+  // Kick off polling when we've found our first plug:
+  if (plugs.length == 1) {
+    setInterval(poll, program.interval * 1000);
+    poll();
+  }
 });
