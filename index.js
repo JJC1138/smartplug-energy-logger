b56fc2791b4fe9d07c7f682da90ebb84a2974ad0
Jon Colverson <github@vcxz.co.uk>
2018-02-09 00:03:31 +0000

    Added beginnings of a '--live-averages' mode that will show moving averages of the readings rather than logging them

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
 #!/usr/bin/env node
 
+const charm = require('charm')();
 const program = require('commander');
 const csvWriter = require('csv-write-stream');
 const { Client } = require('tplink-smarthome-api');
 
 program
   .option('--interval <n>', 'The time in seconds between readings', parseFloat, 0.5)
+  .option('--live-averages')
   .parse(process.argv);
 
 const client = new Client();
 
 let plugs = [];
 
+charm.pipe(process.stdout);
+
 const csv = csvWriter();
 csv.pipe(process.stdout);
 
+let lastPollLines = 0;
+
 function poll() {
+  let out = '';
+  let readingPromises = [];
+
   for (let plug of plugs) {
-    plug.emeter.getRealtime().then((readings) => {
-      csv.write({
-        timestamp: new Date().toISOString(),
-        alias: plug.alias,
-        power: readings.power
-      });
+    let promise = plug.emeter.getRealtime();
+    promise.then((readings) => {
+      if (program.liveAverages) {
+        out += plug.alias + '\n';
+        out += readings.power.toString() + '\n';
+      } else {
+        csv.write({
+          timestamp: new Date().toISOString(),
+          alias: plug.alias,
+          power: readings.power
+        });
+      }
+    });
+    readingPromises.push(promise);
+  }
+
+  if (program.liveAverages) {
+    Promise.all(readingPromises).then(() => {
+      charm.move(0, -lastPollLines);
+      charm.erase('down');
+      charm.write(out);
+      lastPollLines = out.split('\n').length - 1;
     });
   }
 }
